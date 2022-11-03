3ceffa5e2fc135e934e6f7eff2f14e6efbabab35
streamlet <streamlet@outlook.com>
2022-11-04 01:17:12 +0800

    add samples for win

--- src/shared_library.cc
+++ src/shared_library.cc
@@ -1,6 +1,6 @@
 #include "shared_library.h"
 #include <stdio.h>
 
-int shared_library_api() {
+SHARED_LIBRARY_API int shared_library_api() {
   return printf("Hello, world! (from shared library)\n");
 }
--- src/shared_library.h
+++ src/shared_library.h
@@ -1,3 +1,13 @@
 #pragma once
 
-int shared_library_api();
+#ifdef _WIN32
+#ifdef SHARED_LIBRARY_EXPORTING
+#define SHARED_LIBRARY_API __declspec(dllexport)
+#else
+#define SHARED_LIBRARY_API __declspec(dllimport)
+#endif
+#elif
+#else
+#endif
+
+SHARED_LIBRARY_API int shared_library_api();
